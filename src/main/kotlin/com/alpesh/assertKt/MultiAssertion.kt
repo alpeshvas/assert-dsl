1f2ce66b401d40e2b1113739a6ad486af9f5684b
alpesh <alpeshvash@gmail.com>
2022-07-16 18:08:04 +0530

    Add multi checks

--- src/main/kotlin/com/alpesh/assertKt/MultiAssertion.kt
+++ src/main/kotlin/com/alpesh/assertKt/MultiAssertion.kt
@@ -1,7 +1,13 @@
 package com.alpesh.assertKt
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
-
+data class Check(val a: Any, val b: Any, val message: String? = null)
+open class MultiCheckDSL {
+    val checks = mutableListOf<Check>()
+    fun check(expected: Any, actual: Any, message: String? = null) {
+        checks.add(Check(expected, actual, message))
+    }
+}
 open class MultiAssertionDSL {
     private val assertionsList = mutableListOf<() -> Unit>()
     fun <T> equals(expect: T, actual: T, message: String? = null) {
@@ -11,6 +17,23 @@ open class MultiAssertionDSL {
         assertionsList.add { assertNotEquals(illegal = expect, actual = actual, message) }
     }
 
+    fun equals(builder: MultiCheckDSL.() -> Unit) {
+        val allChecks = MultiCheckDSL().apply { builder() }.checks
+        assertionsList.add {
+            allChecks.forEach {
+                assertEquals(expected = it.a, actual = it.b, message = it.message)
+            }
+        }
+    }
+
+    fun notEquals(builder: MultiCheckDSL.() -> Unit) {
+        val allChecks = MultiCheckDSL().apply { builder() }.checks
+        assertionsList.add {
+            allChecks.forEach {
+                assertNotEquals(illegal = it.a, actual = it.b, message = it.message)
+            }
+        }
+    }
     fun runAssertions() {
         assertionsList.forEach { it() }
     }
--- src/test/kotlin/SampleTest.kt
+++ src/test/kotlin/SampleTest.kt
@@ -1,6 +1,6 @@
 import com.alpesh.assertKt.Sample
-import org.junit.jupiter.api.Test
 import com.alpesh.assertKt.assert
+import org.junit.jupiter.api.Test
 
 internal class SampleTest {
     private val sample = Sample()
@@ -9,6 +9,23 @@ internal class SampleTest {
         assert {
             equals(sample.sum(1, 2), 3)
             notEquals(sample.sum(1, 2), 4)
+        }
+    }
+
+    @Test
+    fun sumWithMultiChecks() {
+        assert {
+            equals {
+                check(sample.sum(1, 2), 3)
+                check(sample.sum(1, 4), 5)
+                check(sample.sum(2, 2), 4)
+            }
+            notEquals {
+                check(sample.sum(1, 2), 4)
+                check(sample.sum(1, 3), 5)
+                check(sample.sum(1, 4), 6)
+                check(sample.sum(2, 2), 5)
+            }
         }
     }
 }
