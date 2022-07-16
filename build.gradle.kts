1d5766f7de3b99cc79fec9ef3e4f170e655a9a35
alpesh <alpeshvash@gmail.com>
2022-07-16 17:32:47 +0530

    Add publish workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,38 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.0"
+    `maven-publish`
     application
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.alpesh.assertKt"
+version = "0.1-SNAPSHOT"
 
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/alpeshvas/assert-dsl")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
 repositories {
     mavenCentral()
 }
 
+// publish to github package repository
 dependencies {
     implementation(kotlin("test"))
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 tasks.test {
